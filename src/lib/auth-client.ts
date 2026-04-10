a8da3a9aafd624673cbfaa0c407c52c143fb5bdc
ascendantecommerce <videos@ascendantecommerce.com>
2026-04-10 00:59:36 -0500

    Update auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,7 @@ import { stripeClient } from "@better-auth/stripe/client";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
+  //BASE_URL
   baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
   plugins: [
     magicLinkClient(),
