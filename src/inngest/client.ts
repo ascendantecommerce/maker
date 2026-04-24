3def9d646933fba6c07591c22a7cd3269d356fa7
Dany Boza <cloud@snapmotion.one>
2026-04-24 14:36:59 -0500

    set inngest env

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -3,4 +3,5 @@ import { Inngest } from "inngest";
 // Create a client to send and receive events with realtime support
 export const inngest = new Inngest({
   id: "my-app",
+  isDev: process.env.NODE_ENV === "development",
 });
